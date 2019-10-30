859246699ea370e9b873961bbe9cf406eff51c2a
Anders Ytterström <yttan@fastmail.se>
2019-10-30 18:57:57 +0100

    Add link to Board games page

--- index.js
+++ index.js
@@ -1,29 +1,29 @@
-var hljs            = require('hilight/index');
-var Metalsmith      = require('metalsmith');
-var atomfeed        = require('metalsmith-feed-atom');
-var collections     = require('metalsmith-collections');
-var dateFormatter   = require('metalsmith-date-formatter');
-var helpers        = require('metalsmith-discover-helpers');
-var htmlMinifier    = require("metalsmith-html-minifier");
-var layouts         = require('metalsmith-layouts');
-var markdown        = require('metalsmith-markdown-remarkable');
-var pagination      = require('metalsmith-pagination');
-var partials        = require('metalsmith-discover-partials');
-var permalinks      = require('metalsmith-permalinks');
-var sitemap         = require('metalsmith-sitemap');
-var static          = require('metalsmith-static');
+var hljs = require("hilight/index");
+var Metalsmith = require("metalsmith");
+var atomfeed = require("metalsmith-feed-atom");
+var collections = require("metalsmith-collections");
+var dateFormatter = require("metalsmith-date-formatter");
+var helpers = require("metalsmith-discover-helpers");
+var htmlMinifier = require("metalsmith-html-minifier");
+var layouts = require("metalsmith-layouts");
+var markdown = require("metalsmith-markdown-remarkable");
+var pagination = require("metalsmith-pagination");
+var partials = require("metalsmith-discover-partials");
+var permalinks = require("metalsmith-permalinks");
+var sitemap = require("metalsmith-sitemap");
+var static = require("metalsmith-static");
 
-var author = function (params) {
-  return function (files, _, done) {
-    Object.keys(files).forEach(function (file) {
+var author = function(params) {
+  return function(files, _, done) {
+    Object.keys(files).forEach(function(file) {
       files[file].author = {
         name: params.name,
         email: params.email
-      }
-    })
+      };
+    });
     done();
-  }
-}
+  };
+};
 
 Metalsmith(__dirname)
   .metadata({
@@ -31,89 +31,116 @@ Metalsmith(__dirname)
     url: "https://madr.se/",
     image: "https://madr.se/mugshot.jpg"
   })
-  .source('./src')
-  .destination('./docs')
+  .source("./src")
+  .destination("./docs")
   .clean(false)
-  .use(collections({
-    events: {
-      sortBy: 'date',
-      reverse: true
-    },
-    "brutal-legends": {
-      sortBy: 'date',
-      reverse: true
-    },
-    articles: {
-      sortBy: 'date',
-      reverse: true
-    },
-    posts: {
-      pattern: '20*/*.md',
-      sortBy: 'date',
-      reverse: true
-    }
-  }))
-  .use(author({
-    name: 'Anders Ytterström',
-    email: 'yttan@fastmail.se'
-  }))
-  .use(pagination({
-    'collections.posts': {
-      perPage: 10,
-      layout: 'list.hbs',
-      path: ':num/index.html'
-    }
-  }))
-  .use(markdown({
-    html: true,
-    typographer:  false,
-    highlight: function (str, lang) {
-      if (lang && lang in hljs.languages) {
-        try {
-          return hljs.highlight(lang, str).value;
-        } catch (__) {}
+  .use(
+    collections({
+      events: {
+        sortBy: "date",
+        reverse: true
+      },
+      "brutal-legends": {
+        sortBy: "date",
+        reverse: true
+      },
+      articles: {
+        sortBy: "date",
+        reverse: true
+      },
+      posts: {
+        pattern: "20*/*.md",
+        sortBy: "date",
+        reverse: true
       }
-  
-      try {
-        return hljs.highlightAuto(str).value;
-      } catch (__) {}
-  
-      return '';
-    }
-  }))
+    })
+  )
+  .use(
+    author({
+      name: "Anders Ytterström",
+      email: "yttan@fastmail.se"
+    })
+  )
+  .use(
+    pagination({
+      "collections.posts": {
+        perPage: 10,
+        layout: "list.hbs",
+        path: ":num/index.html"
+      }
+    })
+  )
+  .use(
+    markdown({
+      html: true,
+      typographer: false,
+      highlight: function(str, lang) {
+        if (lang && lang in hljs.languages) {
+          try {
+            return hljs.highlight(lang, str).value;
+          } catch (__) {}
+        }
+
+        try {
+          return hljs.highlightAuto(str).value;
+        } catch (__) {}
+
+        return "";
+      }
+    })
+  )
   .use(permalinks()) // must be *before* atomfeed, and *after* collections
-  .use(atomfeed({    // must be *after* permalinks
-    collection: 'articles',
-    destination: 'prenumerera.xml',
-    metadata: {
-      title: 'madr.se',
-      subtitle: 'Anders Ytterström om hårdrock, musik och webbutveckling',
-      url: 'https://madr.se/'
-    }
-  }))
-  .use(dateFormatter({
-    dates: [{
-      key: 'date',
-      format: 'YYYY-MM-DD'
-    }]
-  }))
-  .use(sitemap({
-    hostname: 'https://madr.se'
-  }))
-  .use(partials({
-    directory: 'layouts/partials'
-  }))
-  .use(helpers({
-    directory: 'layouts/helpers'
-  }))
-  .use(layouts({
-    engine: 'handlebars'
-  }))
+  .use(
+    atomfeed({
+      // must be *after* permalinks
+      collection: "articles",
+      destination: "prenumerera.xml",
+      metadata: {
+        title: "madr.se",
+        subtitle: "Anders Ytterström om hårdrock, musik och webbutveckling",
+        url: "https://madr.se/"
+      }
+    })
+  )
+  .use(
+    dateFormatter({
+      dates: [
+        {
+          key: "date",
+          format: "YYYY-MM-DD"
+        }
+      ]
+    })
+  )
+  .use(
+    sitemap({
+      hostname: "https://madr.se"
+    })
+  )
+  .use(
+    partials({
+      directory: "layouts/partials"
+    })
+  )
+  .use(
+    helpers({
+      directory: "layouts/helpers"
+    })
+  )
+  .use(
+    layouts({
+      engine: "handlebars"
+    })
+  )
   .use(htmlMinifier())
-  .use(static({
-    "src": "./assets",
-    "dest": "./"
-  }))
+  .use(
+    static({
+      src: "./assets",
+      dest: "./"
+    })
+  )
   .build(function(err, _files) {
-    if (err) { throw err; }
+    if (err) {
+      throw err;
+    }
   });
